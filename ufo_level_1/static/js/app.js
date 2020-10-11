f619e4198c93074173886aac5f8f3ae4dde4ea84
gamem001 <melgamez@gmail.com>
2020-10-11 16:28:22 -0500

    I got the data!!!!

--- ufo_level_1/static/js/app.js
+++ ufo_level_1/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
-var tableData = data;
+let tableData = data;
+
+// Use D3 to select the table body
+let tbody = d3.select('tbody');
+console.log(data);
+
+data.forEach((ufoSighting) => {
+    console.log(ufoSighting);
+    let row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        console.log(key,value);
+        let cell = row.append('td');
+        cell.text(value);
+    });
+});
 
 // YOUR CODE HERE!
+let button = d3.select("filter-btn");
+let form = de.select("datetime");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+
+}
