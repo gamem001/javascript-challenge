7395efa50ad60824981368879cd03d175a52294e
gamem001 <melgamez@gmail.com>
2020-10-11 20:55:03 -0500

    code comments added

--- ufo_level_1/static/js/app.js
+++ ufo_level_1/static/js/app.js
@@ -30,22 +30,23 @@ let form = d3.select(".form-group");
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
+//what should happen when form is submitted/button clicked
 function runEnter() {
     //prevent the page from refreshing
     d3.event.preventDefault();
-
+    //remove what was there before
     tbody.html("");
     //select input element
     let inputElement = d3.select("#datetime");
     //get the property value of input element
     let inputValue = inputElement.property("value");
-
+    //print to console
     console.log(inputValue);
     console.log(tableData);
-
+    //filter the data for datetime entered in form
     let filteredData = tableData.filter (dateData => dateData.datetime === inputValue);
     console.log(filteredData);
-
+    //iterate through data, append row to table, for each entry, append cell to table (to replace previous table)
     filteredData.forEach((search) => {
         console.log(search);
         let row = tbody.append("tr");
